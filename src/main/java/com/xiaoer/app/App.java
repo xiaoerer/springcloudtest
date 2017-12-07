0dd905228a65497e688948d6f545d26b5c3a26ff
xzj <527468075@qq.com>
2017-12-07 23:05:18 +0800

    init

---
+++ src/main/java/com/xiaoer/app/App.java
@@ -0,0 +1,14 @@
+package com.xiaoer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEu
+public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+
+}
